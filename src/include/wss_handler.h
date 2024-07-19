7b19786a0a54751bef2b2948d429265be57a3de7
Roman Razvan <98484095+razvanrmn@users.noreply.github.com>
2024-07-19 13:17:48 +0300

    Add files via upload

--- src/include/wss_handler.h
+++ src/include/wss_handler.h
@@ -8,5 +8,7 @@
 int init_curl(CURL **curl);
 void uninit_curl(CURL *curl);
 int perform_ws_operations(CURL *curl, const uint8_t *send_payload_buffer, size_t send_payload_size);
-int send_command_over_websocket(CURL *curl, const char *command);
+int send_command_over_websocket(const char *url, const uint8_t *command, size_t command_size, char *response_buffer, size_t response_buffer_size);
+int check_curl_result(CURLcode res, CURL *curl, const char *msg);
+
 #endif // WSS_HANDLER_H
