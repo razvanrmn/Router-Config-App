eefd0a920e5675c62dcc43ddc04e9309d184c660
Roman Razvan <98484095+razvanrmn@users.noreply.github.com>
2024-07-30 13:53:15 +0300

    Add files via upload

--- src/include/protobuf_handler.h
+++ src/include/protobuf_handler.h
@@ -4,12 +4,20 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-int create_protobuf_message(
+int pack_protobuf_message(
     uint8_t **buffer,
     size_t *size,
     const char *obj_path,
     const char *param,
     const char *value,
     const int required);
+
+int unpack_protobuf_message(
+    const uint8_t *buffer,
+    size_t size,
+    char **obj_path,
+    char **param,
+    char **value,
+    int *required);
 
 #endif /* PROTOBUF_HANDLER_H */
